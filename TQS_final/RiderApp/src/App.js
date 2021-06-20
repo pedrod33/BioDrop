59dc7551a0f4566bdd0c44b105d8d56171a4c0db
goncalopereira <pereira.goncalo@ua.pt>
2021-06-20 16:34:53 +0100

    Rider App

---
+++ TQS_final/RiderApp/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Layout } from './components/Layout';
+import Rider from './Rider/Rider';
+import Home from './components/Home';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './custom.css'
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+import './index.css';
+
+class App extends Component {
+  static displayName = App.name;
+
+  render () {
+      return (
+  
+      <Layout>
+             <Route exact path='/' component={Home } />
+            <Route exact path='/Rider' component={Rider} />
+
+              </Layout>
+       
+
+              
+         
+    );
+  }
+
+}
+
+export default App;
---
+++ TQS_final/RiderApp/src/components/Home.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+
+const styles = theme => ({
+    root: {
+        minWidth: 275,
+        marginLeft: 15,
+        marginRight: 15,
+        marginTop: 10,
+        marginBottom: 15,
+        paddingBottom: 20
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 30,
+        color: "#536732",
+        fontWeight: "bold",
+    },
+    pos: {
+        marginBottom: 12,
+    },
+    form: {
+        marginTop: 30,
+    }
+
+});
+
+class Home extends Component {
+    static displayName = Home.name;
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div>
+       
+                <h1>Welcome to <span style={{ color: "#536732" }}>BioDrop - Rider</span></h1>
+                <h4>You will be notified of your orders here!</h4>
+                <hr />
+                <Row style={{ backgroundColor: "#ABA243" }}>   
+                    <Col>
+                        <Card className={classes.root} variant="outlined">
+                            <CardContent>
+                                <Typography variant="h3" className={classes.title} gutterBottom>
+                                    Register
+                                </Typography>
+                                <Form className={classes.form}>
+                                    <Form.Group className="mb-3" controlId="formEmail">
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control type="email" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formNome">
+                                        <Form.Label>First name</Form.Label>
+                                        <Form.Control type="text" rows={1} />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formApelido">
+                                        <Form.Label>Last name</Form.Label>
+                                        <Form.Control type="text" rows={1} />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formTelefone">
+                                        <Form.Label>Cellphone number</Form.Label>
+                                        <Form.Control type="number" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formTelefone">
+                                        <Dropdown >
+                                            <Dropdown.Toggle variant="Light" id="dropdown-basic">
+                                                Sex
+                                            </Dropdown.Toggle>
+
+                                            <Dropdown.Menu>
+                                                <Dropdown.Item>Male</Dropdown.Item>
+                                                <Dropdown.Item>Female</Dropdown.Item>
+                                                <Dropdown.Item>Other</Dropdown.Item>
+                                            </Dropdown.Menu>
+                                        </Dropdown>
+                                    </Form.Group>
+                                </Form>
+                            </CardContent>
+                        </Card>
+                    </Col>
+                    <Col> 
+                        <Card className={classes.root} variant="outlined">
+                            <CardContent>
+                                <Typography variant="h3" className={classes.title} color="textSecondary" gutterBottom>
+                                    Login
+                                </Typography>
+                                <Form className={classes.form}>
+                                    <Form.Group className="mb-3" controlId="formEmail">
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control type="email" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" />
+                                    </Form.Group>
+                                </Form>
+                            </CardContent>
+                        </Card>
+                    </Col>
+                    </Row>    
+          </div>
+        );
+    }
+}
+export default withStyles(styles)(Home);
