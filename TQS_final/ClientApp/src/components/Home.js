f60be057ad0962b0d51e98f5dbfef978e4327f0f
josecarlos55 <46457623+josecarlos55@users.noreply.github.com>
2021-06-21 16:31:29 +0100

    Css nos botões e Separação de login e register no rider

--- TQS_final/ClientApp/src/components/Home.js
+++ TQS_final/ClientApp/src/components/Home.js
@@ -1,213 +1,136 @@
-import React, { Component } from "react";
-import { withStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
-import Dropdown from "react-bootstrap/Dropdown";
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import { Link }from "react-router-dom";
 
-import ClientService from "../services/client.service";
+import Form from 'react-bootstrap/Form';
 
 
-const styles = (theme) => ({
-	root: {
-		minWidth: 275,
-		marginLeft: 15,
-		marginRight: 15,
-		marginTop: 10,
-		marginBottom: 15,
-		paddingBottom: 20,
-	},
-	title: {
-		textAlign: "center",
-		fontSize: 30,
-		color: "#536732",
-		fontWeight: "bold",
-	},
-	pos: {
-		marginBottom: 12,
-	},
-	form: {
-		marginTop: 30,
-	},
+const styles = theme => ({
+    root: {
+        width: '80%',
+        marginLeft: 15,
+        marginRight: 15,
+        marginTop: 10,
+        marginBottom: 15,
+        paddingBottom: 20
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 30,
+        color: "#536732",
+        fontWeight: "bold",
+    },
+    pos: {
+        marginBottom: 12,
+    },
+    form: {
+        marginTop: 30,
+    },
+    mainDiv: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "#ABA243"
+    }
+
 });
 
 class Home extends Component {
-	static displayName = Home.name;
+    static displayName = Home.name;
 
-	constructor(props) {
-		super(props);
-		this.state = {
-            user: null
+    constructor(props) {
+        super(props);
+        this.state = { email: '', password: '', fName: '', lName: '', phone: '', sex: '' };
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+    }
+
+    handleSubmit(event) {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                "email": this.state.email,
+                "password": this.state.password,
+            })
         };
-	}
+        fetch('http://localhost:8089/deliveries-api/supervisor/login', requestOptions)
+            .then(response => response.json())
+            .then(data => this.setState({ postId: data.id }));
+        event.preventDefault();
+    }
 
-	login() {
-		var emailInput = document.getElementById("formLoginEmail").value;
-		var passwordInput = document.getElementById("formLoginPassword").value;
+    handleChangeEmail(event) {
+        this.setState({ email: event.target.value });
+    }
 
-		if (passwordInput !== "" && emailInput !== "")
-			ClientService.login(emailInput, passwordInput).then((response) => {
-				console.log(response);
+    handleChangePassword(event) {
+        this.setState({ password: event.target.value });
+    }
 
-				if (response.status === 200) {
-                    //this.setState({user: response})
-                    this.props.history.push({pathname: "/stores", state: { user: response }});
-                } 
-				else console.log(response.message);
-			});
 
-		return;
-	}
-
-	register() {
-		var nameInput = document.getElementById("formRegisterName").value;
-		var emailInput = document.getElementById("formRegisterEmail").value;
-		var passwordInput = document.getElementById(
-			"formRegisterPassword"
-		).value;
-		var phoneNumberInput = document.getElementById(
-			"formRegisterPhoneNumber"
-		).value;
-		var genderInput = "Male"; //document.getElementById("formRegisterGender");
-
-		if (
-			nameInput != null &&
-			emailInput != null &&
-			passwordInput != null &&
-			phoneNumberInput != null &&
-			genderInput != null
-		)
-			ClientService.register(
-				nameInput,
-				emailInput,
-				passwordInput,
-				genderInput,
-				phoneNumberInput
-			).then((response) => {
-				console.log(response);
-
-				if (response.status === 200) console.log(response.client);
-				else console.log(response.message);
-			});
-	}
-
-	render() {
-		const { classes } = this.props;
-		return (
-			<div>
-				<button onClick={() => this.login()}>login</button>
-
-				<button onClick={() => this.register()}>register</button>
-
-				<h1>
-					Welcome to <span style={{ color: "#536732" }}>BioDrop</span>
-				</h1>
-				<h4>Get your groceries deliver to you fresh!</h4>
-				<hr />
-				<Row style={{ backgroundColor: "#ABA243" }}>
-					<Col>
-						<Card className={classes.root} variant="outlined">
-							<CardContent>
-								<Typography
-									variant="h3"
-									className={classes.title}
-									gutterBottom
-								>
-									Register
-								</Typography>
-								<Form className={classes.form}>
-									<Form.Group
-										className="mb-3"
-										controlId="formRegisterEmail"
-									>
-										<Form.Label>Email</Form.Label>
-										<Form.Control type="email" />
-									</Form.Group>
-									<Form.Group
-										className="mb-3"
-										controlId="formRegisterPassword"
-									>
-										<Form.Label>Password</Form.Label>
-										<Form.Control type="password" />
-									</Form.Group>
-									<Form.Group
-										className="mb-3"
-										controlId="formRegisterName"
-									>
-										<Form.Label>First name</Form.Label>
-										<Form.Control type="text" rows={1} />
-									</Form.Group>
-									<Form.Group
-										className="mb-3"
-										controlId="formRegisterPhoneNumber"
-									>
-										<Form.Label>
-											Cellphone number
-										</Form.Label>
-										<Form.Control type="number" />
-									</Form.Group>
-									<Form.Group
-										className="mb-3"
-										controlId="formRegisterGenderGroup"
-									>
-										<Dropdown id="dropdown-basic">
-											<Dropdown.Toggle
-												variant="Light"
-												id="formRegisterGender"
-											>
-												Sex
-											</Dropdown.Toggle>
-
-											<Dropdown.Menu>
-												<Dropdown.Item>
-													Male
-												</Dropdown.Item>
-												<Dropdown.Item>
-													Female
-												</Dropdown.Item>
-											</Dropdown.Menu>
-										</Dropdown>
-									</Form.Group>
-								</Form>
-							</CardContent>
-						</Card>
-					</Col>
-					<Col>
-						<Card className={classes.root} variant="outlined">
-							<CardContent>
-								<Typography
-									variant="h3"
-									className={classes.title}
-									color="textSecondary"
-									gutterBottom
-								>
-									Login
-								</Typography>
-								<Form className={classes.form}>
-									<Form.Group
-										className="mb-3"
-										controlId="formLoginEmail"
-									>
-										<Form.Label>Email</Form.Label>
-										<Form.Control type="email" />
-									</Form.Group>
-									<Form.Group
-										className="mb-3"
-										controlId="formLoginPassword"
-									>
-										<Form.Label>Password</Form.Label>
-										<Form.Control type="password" />
-									</Form.Group>
-								</Form>
-							</CardContent>
-						</Card>
-					</Col>
-				</Row>
-			</div>
-		);
-	}
+    render() {
+        const { classes } = this.props;
+        return (
+            <div>
+                <h1>Welcome to <span style={{ color: "#536732" }}>BioDrop - User</span></h1>
+                <h4>Get your Groceries here!</h4>
+                <hr />
+                <div className={classes.mainDiv}>
+                    <Card className={classes.root} variant="outlined">
+                        <CardContent>
+                            <Typography variant="h3" className={classes.title} color="textSecondary" gutterBottom>
+                                Login
+                            </Typography>
+                            <Form className={classes.form}>
+                                <Form.Group className="mb-3" controlId="formEmail2">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control type="email" onChange={this.handleChangeEmail}  />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formPassword2">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" onChange={this.handleChangePassword}  />
+                                </Form.Group>
+                                <Row style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: 30 }}>
+                                    <input type="submit" value="Login" style={{
+                                        fontWeight: 'bold',
+                                        fontSize: '0.75rem',
+                                        color: 'white',
+                                        backgroundColor: 'green',
+                                        borderRadius: 8,
+                                        padding: '3px 10px',
+                                        display: 'inline-block',
+                                    
+                                    }} />
+                                </Row>
+                            </Form>
+                            <Row style={{display: 'flex', justifyContent: 'center', alignItems: 'center',}}>
+                                <Typography variant="h3" className={classes.title} color="textSecondary" gutterBottom>
+                                    Create an account:
+                                </Typography>
+                                <Link to="/Register" style={{
+                                        fontWeight: 'bold',
+                                        fontSize: '0.75rem',
+                                        color: 'white',
+                                        backgroundColor: 'green',
+                                        borderRadius: 8,
+                                        padding: '3px 10px',
+                                        display: 'inline-block',
+										marginLeft: 15,
+										marginBottom:7
+                                    
+                                    }} className="outline-success">Register</Link>
+                            </Row>
+                        </CardContent>
+                    </Card>
+                </div>
+          </div>
+        );
+    }
 }
 export default withStyles(styles)(Home);
